df98e7638dc4cc4a8f6b0bdefb6141037fb4bd2f
timphp <jttj11@gmail.com>
2016-02-12 16:25:10 +0200

    Base

---
+++ 1.php
@@ -0,0 +1,21 @@
+<form action="1.php" method="post">
+    Name: <input type="text" name="name" /><br>
+    Message: <textarea rows="10" cols="20" name="message"></textarea>
+    <input type="submit" value="Загрузить" />
+</form>
+<?php echo print_r($_POST)
+?>
+<?php
+$all="Name: ".$_POST['name']."\r\n"."Message: ".$_POST['message']."\r\n";
+$files="result.txt";
+if (!$handle = fopen($files, 'a')) {
+    echo "Невозможно открыть файл ($filename)";
+exit;
+}
+if (fwrite($handle, $all) === FALSE) {
+    echo "Невозможно произвести запись в файл ($files)";
+exit;
+} else {
+    echo "Информация успешно записана в файл!";
+}
+?>
